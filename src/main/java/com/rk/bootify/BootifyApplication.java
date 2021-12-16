313e25e2378de082d2c04a50eb285ff85e7aa8a9
Raushan Kumar <raushankumar@Raushans-MacBook-Pro.local>
2021-12-16 13:00:22 +0800

    Initial commit with basic dependencies

---
+++ src/main/java/com/rk/bootify/BootifyApplication.java
@@ -0,0 +1,13 @@
+package com.rk.bootify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootifyApplication.class, args);
+	}
+
+}
